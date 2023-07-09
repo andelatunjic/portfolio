45c551f08ec48247b9688a06e58e93f9bfe16746
andelatunjic <andtunjic@gmail.com>
2023-07-09 21:23:45 +0200

    setup

---
+++ src/utils/theme.js
@@ -0,0 +1,10 @@
+export const fonts = {};
+
+export const colors = {};
+
+export const breakpoints = {
+  tabletSmall: "min-width: 580px",
+  tablet: "min-width: 768px",
+  desktopSmall: "min-width: 1024px",
+  desktop: "min-width: 1440px",
+};
